59e60619651f6821021ac1e19323df390649a9e6
Padin358 <padin358@email.cz>
2025-04-24 00:02:55 +0200

    First Alpha version - v. A100

---
+++ Alpha/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+      content: ["./src/**/*.{html,js}"],
+      theme: {
+        extend: {
+          colors: {
+            'basicBg': 'rgb(0 11 23)',
+            'secBg': 'rgb(0 15 30)',
+          },
+          fontFamily: {
+            'normal': 'JetBrains Mono',
+            'special': 'Inter Space Grotesk',
+          },
+        },
+      },
+      plugins: [],
+    }
